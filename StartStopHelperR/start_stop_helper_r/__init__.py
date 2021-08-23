377604bb2ebfc2cdccde82b800d4c0a76a4ae454
zhang_anzhi <zhang_anzhi@126.com>
2021-08-23 14:56:21 +0800

    feat(StartStopHelperR): support MCDR 2.0

---
+++ StartStopHelperR/start_stop_helper_r/__init__.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+from typing import Dict
+
+from mcdreforged.plugin.server_interface import PluginServerInterface
+from mcdreforged.api.command import *
+from mcdreforged.api.utils import Serializable
+
+
+class Config(Serializable):
+    permissions: Dict[str, int] = {
+        'help': 3,
+        'start': 3,
+        'stop': 3,
+        'stop_exit': 4,
+        'restart': 3,
+        'exit': 4,
+    }
+
+
+config: Config
+
+
+def on_load(server: PluginServerInterface, old):
+    global config
+    config = server.load_config_simple('config.json', target_class=Config)
+    permissions = config.permissions
+    server.register_help_message(
+        '!!server',
+        {
+            'en_us': 'Start and stop server helper',
+            'zh_cn': '开关服助手'
+        }
+    )
+    server.register_command(
+        Literal('!!server').
+            requires(lambda src: src.has_permission(permissions['help'])).
+            runs(
+            lambda src: src.reply(server.tr('start_stop_helper_r.help'))
+        ).
+            then(
+            Literal('start').
+                requires(lambda src: src.has_permission(permissions['start'])).
+                runs(lambda src: server.start())
+        ).
+            then(
+            Literal('stop').
+                requires(lambda src: src.has_permission(permissions['stop'])).
+                runs(lambda src: server.stop())
+        ).
+            then(
+            Literal('stop_exit').
+                requires(
+                lambda src: src.has_permission(permissions['stop_exit'])).
+                runs(lambda src: server.stop_exit())
+        ).
+            then(
+            Literal('restart').
+                requires(
+                lambda src: src.has_permission(permissions['restart'])).
+                runs(lambda src: server.restart())
+        ).
+            then(
+            Literal('exit').
+                requires(lambda src: src.has_permission(permissions['exit'])).
+                runs(lambda src: server.exit())
+        )
+    )
