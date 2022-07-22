22c74d6d08a49445e3f0cb8e9f4b7ebcd6ecda94
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2022-07-22 10:08:35 +0800

    fix(bot): 🐛 pass arg for raise exception
    Raise IllegalDimensionException in the save method in BotManager.

--- bot/bot/bot_manager.py
+++ bot/bot/bot_manager.py
@@ -244,7 +244,7 @@ class BotManager:
         else:
             # Check dimension
             if location.dimension is None:
-                raise IllegalDimensionException
+                raise IllegalDimensionException(location.dimension)
 
             if self.is_in_list(name):
                 bot = self.get_bot(name)
