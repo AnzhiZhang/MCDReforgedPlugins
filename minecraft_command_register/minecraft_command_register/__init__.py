200604503d80543cecaa85de87194dca14443956
Andy Zhang <37402126+AnzhiZhang@users.noreply.github.com>
2022-11-20 16:57:25 +0000

    feat(minecraft_command_register): 🎉 new plugin minecraft_command_register

---
+++ minecraft_command_register/minecraft_command_register/__init__.py
@@ -0,0 +1,66 @@
+import json
+
+from mcdreforged.api.types import PluginServerInterface
+from mcdreforged.api.event import MCDRPluginEvents
+from mcdreforged.api.decorator import event_listener
+from mcdreforged.mcdr_server import MCDReforgedServer
+from mcdreforged.plugin.plugin_registry import PluginCommandHolder
+
+mcdr_server: MCDReforgedServer
+
+
+class Node:
+    def __init__(self, node):
+        self.children = {}
+
+        # Argument children
+        for argument_child in node._children:
+            child_name = f'<{argument_child._ArgumentNode__name}>'
+            self.children[child_name] = Node(argument_child)
+
+        # Literal children
+        for key, literal_children in node._children_literal.items():
+            self.children[key] = Node(literal_children[0])
+
+    @property
+    def dict(self):
+        if self.children:
+            return {key: value.dict for key, value in self.children.items()}
+        else:
+            return None
+
+
+def register(server: PluginServerInterface):
+    # return if server is not startup
+    if not server.is_server_startup():
+        return
+
+    # get tree data
+    root_nodes = mcdr_server.command_manager.root_nodes
+    tree_data = {}
+    for key, value in root_nodes.items():
+        plugin_command_holder: PluginCommandHolder = value[0]
+        tree_data[key] = Node(plugin_command_holder.node).dict
+
+    # execute register command
+    server.execute_command(f'/mcdr register {json.dumps(tree_data)}')
+    return tree_data
+
+
+@event_listener(MCDRPluginEvents.PLUGIN_LOADED, priority=0)
+def on_load_event(server: PluginServerInterface, prev_module):
+    global mcdr_server
+    mcdr_server = server._mcdr_server
+
+    # Call register when register command
+    old_on_plugin_registry_changed = mcdr_server.on_plugin_registry_changed
+
+    def new_on_plugin_registry_changed():
+        old_on_plugin_registry_changed()
+        register(server)
+
+    mcdr_server.on_plugin_registry_changed = new_on_plugin_registry_changed
+
+
+def on_server_startup(server: PluginServerInterface):
+    register(server)
---
+++ minecraft_command_register/readme.md
@@ -0,0 +1,11 @@
+# Minecraft Command Register
+
+> 在 Minecraft 中注册 MCDReforged 指令
+
+例：`!!qb` 将被注册为 `/qb`
+
+## 安装指南
+
+### Fabric
+
+需同时安装 xxx 模组
