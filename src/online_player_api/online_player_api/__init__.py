497a4340bfda9b32b4a1848b882b51eb33227f94
Kevin Z <zyxkad@gmail.com>
2024-09-02 14:22:51 -0600

    online_player_api: make check_online case insensitive

--- src/online_player_api/online_player_api/__init__.py
+++ src/online_player_api/online_player_api/__init__.py
@@ -1,34 +1,36 @@
 from typing import List
 
-online_players = []
+online_players = {}
 
 
 def on_load(server, old):
     global online_players
     if old is not None and hasattr(old, 'online_players'):
-        online_players = old.online_players
+        if isinstance(old.online_players, dict):
+            online_players = old.online_players
+        elif isinstance(old.online_players, list):
+            for player in old.online_players:
+                online_players[player.lower()] = player
 
 
 def on_server_stop(server, return_code):
     global online_players
-    online_players = []
+    online_players = {}
 
 
 def on_player_joined(server, player, info):
-    if player not in online_players:
-        online_players.append(player)
+    online_players[player.lower()] = player
 
 
 def on_player_left(server, player):
-    if player in online_players:
-        online_players.remove(player)
+    online_players.pop(player.lower(), None)
 
 
 def check_online(player: str) -> bool:
     """Check a player is online"""
-    return player in online_players
+    return player.lower() in online_players
 
 
 def get_player_list() -> List[str]:
     """Get all online player list"""
-    return online_players.copy()
+    return list(online_players.values())
